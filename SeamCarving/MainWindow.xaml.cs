3b4eb03725ce1c389e4942b76a09b3f114f8cf8b
János Jung <janos.jung@outlook.com>
2017-11-20 08:55:08 +0100

    -Added an OpenFileDialog to load image -Image loads according to the OpenFileDialog result and is displayed as a BitmapImage -Image is loaded as a Bitmap object for later pixel-to-pixel calculations -Added image size info in MainWindow

--- SeamCarving/MainWindow.xaml.cs
+++ SeamCarving/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace SeamCarving
 {
@@ -20,14 +22,53 @@ namespace SeamCarving
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Controls.Image image;
+        Bitmap bitmap;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var mainWindow = Application.Current.MainWindow;
+            mainWindow.SizeToContent = SizeToContent.WidthAndHeight;
+
+        }
+
+        private void updateSizeDisplay()
+        {
+            widthLabel.Content = "Width: " + bitmap.Width + "pixels";
+            heightLabel.Content = "Height: " + bitmap.Height + "pixels";
         }
 
         private void clickedFileOpen(object sender, RoutedEventArgs e)
         {
 
+            // Displays an Open File Dialog to load an image for display and edit              
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.DefaultExt = ".jpg";
+            openFileDialog.Filter = "Image documents (*.bmp;*.png;*.jpeg;*.jpg)|*.bmp;*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            openFileDialog.ShowDialog();
+
+
+            // Loads the image and displays it 
+            
+            image = new System.Windows.Controls.Image();
+            image.Source = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.RelativeOrAbsolute));
+            image.Stretch = Stretch.Uniform;            
+            ImageControl.Source = image.Source;
+
+
+            // loads same file as bitmap (BMP) to memory to work on
+
+            bitmap = new Bitmap(openFileDialog.FileName);
+            
+            updateSizeDisplay();
+            
+
+
+            
         }
     }
 }
