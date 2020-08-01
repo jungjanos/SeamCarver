619e76baba2035e6191403b7c89ae340cee79325
Jung Janos <janos.jung@outlook.com>
2020-08-01 11:27:27 +0200

    made Index page prettier

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -57,7 +57,7 @@ namespace WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Image}/{action=Index}");
             });
         }
     }
