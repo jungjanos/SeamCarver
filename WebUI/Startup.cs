7b36c67a1430d58ad0bc838ab05e461a42c59c37
Jung Janos <janos.jung@outlook.com>
2020-10-26 23:56:05 +0100

    Added AzureAD authentication from code sample

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -1,9 +1,14 @@
 using System.IO;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using WebUI.Service;
 
 namespace WebUI
@@ -20,7 +25,20 @@ namespace WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"), subscribeToOpenIdConnectMiddlewareDiagnosticsEvents: true);
+
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddRazorPages()
+                .AddMicrosoftIdentityUI();
+
             services.AddSingleton(typeof(FileSystemHelper), sp =>
             {
                 var webRoot = ((IWebHostEnvironment)sp.GetRequiredService<IWebHostEnvironment>()).WebRootPath;
@@ -28,7 +46,7 @@ namespace WebUI
                 var uploadBasePhysical = Path.Combine(webRoot, uploadBaseVirtual);
                 return new FileSystemHelper(uploadBasePhysical, uploadBaseVirtual);
             });
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,15 +62,28 @@ namespace WebUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
+            if (env.IsDevelopment())
+            {// for debugging http message flow
+                app.Use((context, next) =>
+               {
+                   return next.Invoke();
+               });
+            }
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Image}/{action=Index}");
+                endpoints.MapRazorPages();
             });
         }
     }
