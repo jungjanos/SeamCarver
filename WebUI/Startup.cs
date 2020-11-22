9e536521715236fe73a27d88e32e47a1eff5e539
Jung Janos <janos.jung@outlook.com>
2020-11-22 20:34:23 +0100

    Settings for Azure deployment

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -111,7 +111,7 @@ namespace WebUI
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Image/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
